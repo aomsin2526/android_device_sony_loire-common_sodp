b8531028b123efc0b0328a4ab77a4bcb0045ecc8
Wei Wang <wvw@google.com>
2017-07-12 11:25:48 -0700

    loire-common: camera: Do not use perfd by default
    Add persist.camera.perfd.enable to disable/enable perfd connection from
    CameraHAL.
    
    Bug: 63589458
    Test: Test powerHAL hint working with camera and there is no perfd interaction
    Change-Id: Ic288a494be995f24cc85ceb69539aeb0a2d174c3

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -157,7 +157,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
-LOCAL_SHARED_LIBRARIES += libcutils libdl libhal_dbg
+LOCAL_SHARED_LIBRARIES += libbase libcutils libdl libhal_dbg
 ifeq ($(IS_QC_BOKEH_SUPPORTED),true)
 LOCAL_SHARED_LIBRARIES += libdualcameraddm
 LOCAL_CFLAGS += -DENABLE_QC_BOKEH
