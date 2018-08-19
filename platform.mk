92f711ec3bb62f976c3c8927a3d3d49c52992bea
Chippa-a <bagirovvusal99@gmail.com>
2018-08-19 21:50:21 +0300

    loire-common: Switch to Snap camera app
    Change-Id: Ibcd58a7ff1f058379dfa3bb4db037ed1d3d458f3

--- platform.mk
+++ platform.mk
@@ -101,6 +101,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    Snap \
     vendor.qti.hardware.camera.device@1.0
 
 # Charger
