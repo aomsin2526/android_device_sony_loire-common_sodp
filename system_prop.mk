8ec7afcfce703b70f0e89e168193aae24eca1f77
Ady Abraham <adyabr@google.com>
2019-07-24 11:05:44 -0700

    loire-common: enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8

--- system_prop.mk
+++ system_prop.mk
@@ -130,6 +130,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.enable.sglscale=1 \
     debug.mdpcomp.logs=0 \
+    debug.sf.enable_gl_backpressure=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
