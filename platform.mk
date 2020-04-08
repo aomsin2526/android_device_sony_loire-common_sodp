9de0f1d0b84aa84d1d4e96bec2df12ed049e9a4c
Roman Kiryanov <rkir@google.com>
2020-04-08 14:23:47 -0700

    loire-common: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead
    
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307

--- platform.mk
+++ platform.mk
@@ -67,8 +67,8 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
     audio.a2dp.default \
     audio.primary.msm8952 \
     audio.r_submix.default \
