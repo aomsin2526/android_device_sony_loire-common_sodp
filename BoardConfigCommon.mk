fbadaa12387396c63da471f0fa8300d53495b3f8
Chippa-a <bagirovvusal99@gmail.com>
2020-09-12 18:34:57 +0300

    loire-common: Switch to wifi_ap_mac_randomization_supported config
    WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION is
    deprecated, use new overlay config instead.
    
    Change-Id: I8836a969dd53a303db2ea1777536fed155b77f93

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -185,7 +185,6 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/wlan/parameters/fwpath"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/wlan/bcmdhd/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/wlan/bcmdhd/fw_bcmdhd.bin"
-WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
 
