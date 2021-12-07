a756f9774cfd572abb3f24254f0bc962291f86f7
SamarV-121 <samarvispute121@pm.me>
2021-12-07 20:22:48 +0530

    loire-common: Remove obsolete debug.sf.disable_backpressure property
     * It has been removed in Android S
     * https://github.com/LineageOS/android_frameworks_native/commit/2ec5391acfc340c2e195d573cf5a078ff0f280d2
    
    Change-Id: I23a210a37ad257f0c8b7a3c5e1d59b2ac20cde9a
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- system_prop.mk
+++ system_prop.mk
@@ -130,7 +130,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.enable.sglscale=1 \
     debug.mdpcomp.logs=0 \
-    debug.sf.disable_backpressure=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
