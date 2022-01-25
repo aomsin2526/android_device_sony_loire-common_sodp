c347e7ddaaae6ed656ab34838f73d911b0b91da4
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    loire-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- platform.mk
+++ platform.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     audio.primary.msm8952 \
     audio.r_submix.default \
