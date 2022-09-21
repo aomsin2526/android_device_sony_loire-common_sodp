8788e4b78a854bffcd928d8818cb9c787ce8843f
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    loire-common: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- platform.mk
+++ platform.mk
@@ -117,10 +117,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # ConfigStore
 PRODUCT_PACKAGES += \
     disable_configstore
