4cd8fa15576521bcad87f3ec7bfc7e30ce2f3cfb
Bruno Martins <bgcngm@gmail.com>
2022-10-13 16:25:00 +0100

    loire-common: Migrate to lib{gui,ui} shims in hardware/lineage/compat
    Change-Id: I78fce833c8429f5c7dcbf56fd6ba0ffc6159a424

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,12 +175,7 @@ PRODUCT_PUBLIC_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/public
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /vendor/bin/imsrcsd|libbase_shim.so \
-    /system_ext/lib/lib-imscamera.so|libgui_shim.so \
-    /system_ext/lib/lib-imsvideocodec.so|libui_shim.so \
-    /system_ext/lib64/lib-imscamera.so|libgui_shim.so \
-    /system_ext/lib64/lib-imsvideocodec.so|libui_shim.so \
-    /system_ext/lib64/lib-imsvt.so|libgui_shim.so
+    /vendor/bin/imsrcsd|libbase_shim.so
 
 # Vendor SPL
 VENDOR_SECURITY_PATCH = "2018-09-01"
