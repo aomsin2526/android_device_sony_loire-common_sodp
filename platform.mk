05576fa234ef4f4063c7e664ae3442be7f188e62
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    loire-common: Build lineage health HAL and related sepolicies
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad
    Signed-off-by: Chippa-a <bagirovvusal99@gmail.com>

--- platform.mk
+++ platform.mk
@@ -233,6 +233,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.loire
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
