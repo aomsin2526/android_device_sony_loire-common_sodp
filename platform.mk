8d7de8a6c0662e20a70a4cdf7f25e858955406fe
Chippa-a <bagirovvusal99@gmail.com>
2023-08-13 22:45:00 +0300

    loire-common: Update QTI perf blobs from PAN_sprout
    Change-Id: Ic2fe19866cd5fd476ead5c2461925ab2f2df9d2a

--- platform.mk
+++ platform.mk
@@ -282,7 +282,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-loire \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
