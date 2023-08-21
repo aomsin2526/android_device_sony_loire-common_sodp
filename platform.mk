e5936b461a671f2b16895eb2310cb03a92f4fe94
Chippa-a <bagirovvusal99@gmail.com>
2023-08-21 20:26:18 +0300

    loire-common: Update IRQ balance blob from FP3
    Change-Id: I1444eb8a618fd92938eb37619c031ccb768ce42c

--- platform.mk
+++ platform.mk
@@ -213,7 +213,7 @@ PRODUCT_PACKAGES += \
 
 # IRQ Balance
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
 
 # Keylayout
 PRODUCT_COPY_FILES += \
